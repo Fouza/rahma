f51d08987bb46eca4c438ca3939465e2f937c825
Fouza <gf_oukacha@esi.dz>
2020-03-13 16:18:04 +0100

    lol3

--- resources/views/finance.blade.php
+++ resources/views/finance.blade.php
@@ -39,7 +39,7 @@
               </li>
               <li class="nav-item">
                 <a href="#" class="nav-link">
-                  Chiffre semnaine dernière <span class="float-right badge bg-success">1200$</span>
+                  Chiffre semaine dernière <span class="float-right badge bg-success">1200$</span>
                 </a>
               </li>
               <li class="nav-item">
